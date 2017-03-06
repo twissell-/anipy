42951ef4da3586eb03eb5343631251f414c2beca
demo <dmaggioesne@gmail.com>
2017-03-06 03:10:36 -0300

    added some enums. Started coding browser functionality. Not working yet.

--- anipy/ani/enum_.py
+++ anipy/ani/enum_.py
@@ -31,3 +31,50 @@ class Smiley(Enum):
 
     def __str__(self):
         return self.value
+
+
+class Season(Enum):
+    """Enumeration for entry's list status."""
+    winter = 'winter'
+    spring = 'spring'
+    summer = 'summer'
+    fall = 'fall'
+
+class MediaType(Enum):
+    tv = 0
+    tvShort = 1
+    movie = 2
+    special = 3
+    ova = 4
+    ona = 5
+    music = 6
+    manga = 7
+    novel = 8
+    oneShot = 9
+    doujin = 10
+    manhua = 11
+    manhwa = 12
+
+
+class AnimeStatus(Enum):
+    finishedAiring = 'finished airing'
+    currentlyAiring = 'currently airing'
+    notYetAired = 'not yet aired'
+    cancelled = 'cancelled'
+
+
+class Mangatatus(Enum):
+    finishedPublishing = 'finished publishing'
+    publishing = 'publishing'
+    notYetPublished = 'not yet published'
+
+# TODO: genres and genres_exclude goes here
+
+
+class SortBy(Enum):
+    id = 'id'
+    score = 'score'
+    popularity = 'popularity'
+    startDate = 'start_date'
+    endDate = 'end_date'
+    
